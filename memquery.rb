e2380ae5b038a6efa188971c476cb160b0497eb8
buddhamagnet <buddhamagnet@gmail.com>
2013-04-20 14:38:13 +0100

    renamed some methods, allow to run as object or command line script

--- memquery.rb
+++ memquery.rb
@@ -6,24 +6,23 @@ class Memquery
   MEMQUERY_PORT_STANDARD = 11211
   MEMQUERY_PORT_DURABLE = 11226
 
-  attr_reader :cache_dump_limit, :localhost
-  attr_accessor :slab_ids, :port, :regex, :host
+  attr_accessor :cache_dump_limit, :host, :machine, :port, :regex, :slab_ids
 
   def self.version 
     '1.0.0'
   end
 
-  def initialize
-    select_bin
-    set_host
-    set_pattern
+  def initialize(bin, hostname, key = nil)
+    set_port(bin)
+    set_host(hostname)
+    set_pattern(key)
+    set_machine
     @cache_dump_limit = 100
-    set_localhost
     @slab_ids = []
   end
 
-  def select_bin
-    self.port = case ARGV[0]
+  def set_port(bin)
+    self.port = case bin
       when 'd' then MEMQUERY_PORT_DURABLE
       when 'n' then MEMQUERY_PORT_STANDARD
       else
@@ -32,24 +31,24 @@ class Memquery
     end
   end
 
-  def set_host
-    self.host = case ARGV[1]
+  def set_host(hostname)
+    self.host = case hostname
       when 'l' then 'localhost'
-      else ARGV[1]
+      else hostname
     end
   end
 
-  def set_pattern
-    pattern = ARGV[2] || '.+'
+  def set_pattern(key)
+    pattern = key || '.+'
     self.regex = Regexp.new(pattern)
   end
 
-  def set_localhost
-    self.localhost = Net::Telnet::new('Host' => host, 'Port' => port, 'Timeout' => 3)
+  def set_machine
+    self.machine = Net::Telnet::new('Host' => host, 'Port' => port, 'Timeout' => 3)
   end
 
   def get_slabs
-    localhost.cmd("String" => "stats items", "Match" => /^END/) do |c|
+    machine.cmd("String" => "stats items", "Match" => /^END/) do |c|
       matches = c.scan(/STAT items:(\d+):/)
       self.slab_ids = matches.flatten.uniq
     end
@@ -60,7 +59,7 @@ class Memquery
     puts "Expires At\t\t\t\tCache Key"
     puts '-'* 80 
     slab_ids.each do |slab_id|
-      localhost.cmd("String" => "stats cachedump #{slab_id} #{cache_dump_limit}", "Match" => /^END/) do |c|
+      machine.cmd("String" => "stats cachedump #{slab_id} #{cache_dump_limit}", "Match" => /^END/) do |c|
         matches = c.scan(/^ITEM (.+?) \[(\d+) b; (\d+) s\]$/).each do |key_data|
           (cache_key, bytes, expires_time) = key_data
           if (cache_key =~ regex)
@@ -80,10 +79,10 @@ class Memquery
   end
 
   def terminate
-    localhost.close  
+    machine.close  
   end
 end
 
 if ARGV.size > 0
-  puts Memquery.new.run
+  puts Memquery.new(*ARGV).run
 end
